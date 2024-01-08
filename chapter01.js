b6ffb89ed690e2b0b6f9c2f8071a9534cfd8652d
Batman <mrwaynefromgotham@gmail.com>
2024-01-08 07:55:09 +0530

    JS practice set Chapter01

---
+++ chapter01.js
@@ -0,0 +1,33 @@
+//Q1
+let a ="faraaz";
+let k =1
+console.log(a+k);
+
+//Q2
+
+console.log(typeof(a+k));
+
+//Q3
+
+const b = {
+    name : "batman",
+    occupation : "savior",
+    color : "black",
+}
+console.log(b)
+//b= "joker"  cannot add a key to the constant variable later like this.
+
+//Q4
+b["Kills"] = 9,
+b["NAME"] = "The Joker",
+b["name"] = "The Joker",
+console.log(b)
+
+//Q5
+ const dict ={
+    Boy : "sabiun",
+    Girl : "sabiyatun",
+    Man : "rajulun"
+ }
+ console.log(dict)
+ console.log(dict.Boy)
