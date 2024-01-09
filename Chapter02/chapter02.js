9b76968c729f1fa9dd49f4ca8d5d0e1f1ef8a445
Batman <mrwaynefromgotham@gmail.com>
2024-01-09 23:23:55 +0530

    Did question number one of chapter 2

---
+++ Chapter02/chapter02.js
@@ -0,0 +1,14 @@
+//Q1
+let age = prompt("What is your age?")
+
+if (age>10 && age<20)
+{
+    console.log("The age lies in the specified range")
+}
+else
+{
+    console.log("Your out of age range")
+}
+
+//Q2
+let SuperHero = prompt("Who's Your favorite Superhero")
