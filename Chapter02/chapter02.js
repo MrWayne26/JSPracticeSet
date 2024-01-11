a3a9dc8f9454274efd4857e6a621dcc847710293
Batman <mrwaynefromgotham@gmail.com>
2024-01-11 23:54:53 +0530

    Done third Question

--- Chapter02/chapter02.js
+++ Chapter02/chapter02.js
@@ -19,3 +19,12 @@ switch (SuperHero)
     case Joker:
         console.log("The joker is not a superhero")
 }
+
+//Q3
+let number = 12;
+
+if (number % 2 === 0 && number % 3 === 0) {
+    console.log(`${number} is divisible by both 2 and 3.`);
+} else {
+    console.log(`${number} is not divisible by both 2 and 3.`);
+}
